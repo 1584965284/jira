690e81ed114d1528c03a3b838bdc41b8e22fc128
1584965284 <1584965284@qq.com>
2022-07-24 01:48:18 +0800

    new

---
+++ src/screens/kanban/index.tsx
@@ -0,0 +1,7 @@
+export const KanbanScreen = () => {
+  return (
+    <div>
+      <h1>看板</h1>
+    </div>
+  );
+};
---
+++ src/screens/kanban/kanban-column.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { Kanban } from "types/kanban";
+export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {};
